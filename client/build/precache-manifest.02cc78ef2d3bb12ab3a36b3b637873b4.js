25ae61b3d0f61ecc2dfcd442fb3abbe6c070075e
Trie0037 <trie0037@gmail.com>
2020-02-15 19:39:13 -0600

    clibuild

---
+++ client/build/precache-manifest.02cc78ef2d3bb12ab3a36b3b637873b4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "dc4a91ef634398d48da2",
+    "url": "/static/js/main.dc4a91ef.chunk.js"
+  },
+  {
+    "revision": "8006155858e5be10ed4c",
+    "url": "/static/js/1.80061558.chunk.js"
+  },
+  {
+    "revision": "dc4a91ef634398d48da2",
+    "url": "/static/css/main.e95c02bf.chunk.css"
+  },
+  {
+    "revision": "44d391d95103418bb4778a0681a73000",
+    "url": "/index.html"
+  }
+];
