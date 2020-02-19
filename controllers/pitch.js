d8f7649cc65dd548d6035f02e111eeef34987791
Meng Vang <meng.vang92@yahoo.com>
2020-02-18 22:28:11 -0600

    sort projects list from newest to oldest by date

--- controllers/pitch.js
+++ controllers/pitch.js
@@ -1,39 +1,40 @@
 var Pitch = require("../models/Pitch");
 
 module.exports = {
-  insert: function(req, res) {
+  insert: function (req, res) {
     Pitch.create(req.body)
-      .then(function(doc) {
+      .then(function (doc) {
         res.json(doc);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         res.json(err);
       });
   },
-  getAllProjects: function(req, res) {
+  getAllProjects: function (req, res) {
     Pitch.find(req.query)
-      .then(function(doc) {
+      .sort({ date: -1 })
+      .then(function (doc) {
         res.json(doc);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         res.json(err);
       });
   },
-  upVote: function(req, res) {
+  upVote: function (req, res) {
     Pitch.updateOne({ _id: req.params.projectId }, { $inc: { upvote: 1 } })
-      .then(function(doc) {
+      .then(function (doc) {
         res.json(doc);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         res.json(err);
       });
   },
-  downVote: function(req, res) {
+  downVote: function (req, res) {
     Pitch.updateOne({ _id: req.params.projectId }, { $inc: { downvote: -1 } })
-      .then(function(doc) {
+      .then(function (doc) {
         res.json(doc);
       })
-      .catch(function(err) {
+      .catch(function (err) {
         res.json(err);
       });
   }
