83fd64eb3d44044a7a2ac7d4bcab30cbb37eaf53
Trie0037 <trie0037@gmail.com>
2020-02-18 23:30:17 -0600

    relocate

---
+++ build/precache-manifest.08d8c4f9af29e205a46edce57554f3d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1489243be860ec7f7b92",
+    "url": "/Afterclass/static/js/runtime~main.1489243b.js"
+  },
+  {
+    "revision": "bb2e54dae2f0842f7784",
+    "url": "/Afterclass/static/js/main.bb2e54da.chunk.js"
+  },
+  {
+    "revision": "6ac1892418164e5ac723",
+    "url": "/Afterclass/static/js/1.6ac18924.chunk.js"
+  },
+  {
+    "revision": "bb2e54dae2f0842f7784",
+    "url": "/Afterclass/static/css/main.79613f03.chunk.css"
+  },
+  {
+    "revision": "cbae70b3aa20735aa379b21e533a1f03",
+    "url": "/Afterclass/index.html"
+  }
+];
