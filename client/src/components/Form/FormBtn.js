976cb1068e11c54a70cf21426043da2a20401299
Trie0037 <trie0037@gmail.com>
2020-03-22 14:42:47 -0500

    button adjustments

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 export const FormBtn = props => (
-  <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
+  <button {...props} 
+  style={{ float: "right", marginBottom: 10 }} 
+  className="btn btn-success"
+  style={{ flex: "1 1 0%", border: "black" }} >
     {props.children}
   </button>
 );
