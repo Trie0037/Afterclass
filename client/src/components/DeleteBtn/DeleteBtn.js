8f730b6dbcbbfe3359571f9c665ea60a4b59e49b
Trie0037 <trie0037@gmail.com>
2020-03-22 11:25:22 -0500

    removed unnecessary components

--- client/src/components/DeleteBtn/DeleteBtn.js
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// import "../../App.css";
-// const DeleteBtn = props => (
-//   <span className="delete-btn" {...props}>
-//     ✗
-//   </span>
-// );
-
-// export default DeleteBtn;
