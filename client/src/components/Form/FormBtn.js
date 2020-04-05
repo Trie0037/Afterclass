131fe0d2da87d24d0f7666c7c9a2936d8f9f469e
Trie0037 <trie0037@gmail.com>
2020-04-05 14:10:39 -0500

    loosen up some buttons

--- client/src/components/Form/FormBtn.js
+++ client/src/components/Form/FormBtn.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 export const FormBtn = props => (
-  <button {...props} 
-  style={{ float: "right", marginBottom: 10 }} 
-  className="btn btn-success"
-  style={{ flex: "1 1 0%", border: "black" }} >
+  <button
+    {...props}
+    style={{ float: "right", marginBottom: 10 }}
+    className="btn btn-success"
+    style={{ flex: "1 1 0%", border: "black" }}
+  >
     {props.children}
   </button>
 );
