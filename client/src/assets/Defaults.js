80d1a51c1d72168cf9beeea2d2d935ae1994b3c5
mVang92 <meng.vang92@yahoo.com>
2020-07-11 23:44:32 -0500

    change default project photo

--- client/src/assets/Defaults.js
+++ client/src/assets/Defaults.js
@@ -1,4 +1,4 @@
 export const defaults = {
-    defaultProjectImage: "https://raw.githubusercontent.com/Trie0037/Afterclass/master/client/public/assets/images/react.png",
+    defaultProjectImage: "https://raw.githubusercontent.com/Trie0037/Afterclass/master/client/src/assets/images/afterClassLogo.png",
     invalidBackgroundImageError: "Image URL cannot be blank."
 };
